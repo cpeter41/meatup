b62ab0253cccfff5cdc1e04f70bf8a33686dc227
cpeter41 <chris.steven.peters@gmail.com>
2024-02-28 08:50:11 -0800

    await getAll

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -3,7 +3,7 @@ const { Group } = require("../../db/models");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-    const results = Group.findAll();
+    const results = await Group.findAll();
     console.log(results);
     // res.json(results.dataValues);
 });
