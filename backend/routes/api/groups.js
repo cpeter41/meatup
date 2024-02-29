1434e5c380afd5f55e90a81a51cbbc8dce737fdf
cpeter41 <chris.steven.peters@gmail.com>
2024-02-29 10:11:06 -0800

    fix update group

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -148,11 +148,12 @@ router.post("/", async (req, res, next) => {
 });
 
 router.put("/:groupId", async (req, res, next) => {
+    const { name, about, type, private, city, state } = req.body;
     const { groupId } = req.params;
     const foundGroup = await Group.findByPk(groupId);
     if (!foundGroup) return res.status(404).json({ message: "Group couldn't be found"});
 
-    validateGroupData(req.body, err);
+    validateGroupData(req, err);
 
     foundGroup.name = name;
     foundGroup.about = about;
