7f8b8b3120ef67c4a5f7e2f4c0d9d76c5b675a21
cpeter41 <chris.steven.peters@gmail.com>
2024-04-06 15:59:38 -0700

    event.js change

--- backend/db/models/event.js
+++ backend/db/models/event.js
@@ -36,7 +36,7 @@ module.exports = (sequelize, DataTypes) => {
                 type: DataTypes.INTEGER,
                 allowNull: false,
                 references: { model: "Groups", key: "id" },
-                onDelete: "CASCADE",
+                // onDelete: "CASCADE",
             },
             venueId: {
                 type: DataTypes.INTEGER,
