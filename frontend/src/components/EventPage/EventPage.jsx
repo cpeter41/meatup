fddb9973700c52436f353d0f4c7b9ebde409d9ae
cpeter41 <chris.steven.peters@gmail.com>
2024-04-26 11:37:55 -0700

    event host added

--- frontend/src/components/EventPage/EventPage.jsx
+++ frontend/src/components/EventPage/EventPage.jsx
@@ -1,19 +1,26 @@
 import { useDispatch, useSelector } from "react-redux";
-import { NavLink, useParams, useNavigate } from "react-router-dom";
+import { NavLink, useParams /*useNavigate*/ } from "react-router-dom";
 import { getEventDetails } from "../../store/event";
 import { IMG_NOT_FOUND } from "../../util/util";
-import OpenModalMenuItem from "../Navigation/OpenModalMenuItem";
-import ConfirmModal from "../ConfirmModal/ConfirmModal";
+// import OpenModalMenuItem from "../Navigation/OpenModalMenuItem";
+// import ConfirmModal from "../ConfirmModal/ConfirmModal";
 import "./EventPage.css";
 import { useEffect, useState } from "react";
+import { csrfFetch } from "../../store/csrf";
 
 export default function EventPage() {
     const dispatch = useDispatch();
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     const { eventId } = useParams();
     const [isValidId, setIsValidId] = useState(false);
+    const [host, setHost] = useState("");
 
     let previewImage;
+
+    const event = useSelector((state) => state.events.eventDetails);
+    // const userId = useSelector((state) => state.session.user.id);
+
+    console.log(event);
 
     useEffect(() => {
         if (isNaN(eventId)) setIsValidId(false);
@@ -22,13 +29,20 @@ export default function EventPage() {
         if (isValidId) dispatch(getEventDetails(eventId));
     }, [dispatch, eventId, isValidId]);
 
-    const event = useSelector((state) => state.events.eventDetails);
-    const userId = useSelector((state) => state.session.user.id);
-    
-    // get group info from either a db GET or from cached groups perhaps?
-    // need organizer name, group preview pic, group name, and group private status from group
+    useEffect(() => {
+        const groupId = event && event.groupId;
+        console.log("id: ", groupId);
 
-    console.log(event);
+        async function getHost() {
+            const res = await csrfFetch(`/api/groups/${groupId}`);
+            const obj = await res.json();
+            setHost(
+                [obj.Organizer.firstName, obj.Organizer.lastName].join(" ")
+            );
+        }
+
+        if (groupId) getHost();
+    }, [event]);
 
     if (event && event.EventImages) {
         previewImage = event.EventImages.find(
@@ -42,8 +56,8 @@ export default function EventPage() {
             <span>
                 &lt; <NavLink to="/events">Events</NavLink>
             </span>
-            <h1>{event.name}</h1>
-            <span>Hosted by </span>
+            <h1>{event && event.name}</h1>
+            <span>Hosted by {host}</span>
         </div>
     ) : (
         <h1>Page Not Found</h1>
